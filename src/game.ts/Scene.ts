de323686c700e65e192660944f083dc5300e4df0
SilGuaGaitan <dragonrosadito@gmail.com>
2023-09-19 09:54:50 -0300

    juego 19/9

--- src/game.ts/Scene.ts
+++ src/game.ts/Scene.ts
@@ -1,40 +1,54 @@
 import { Container, Graphics, Sprite } from "pixi.js";
 
 export class Scene extends Container{
-    private carta1:Sprite;
-    constructor(){
+   // public carta1:Sprite;
+    //public back:Graphics;
+    constructor()
+    {
         super();
         const contenedor=new Container()
+        contenedor.position.x=100;
+        contenedor.position.y=50;
         this.addChild(contenedor);
 
-            this.carta1=  Sprite.from("alfajor");
-            this.carta1.scale.set(0.4);
-            this.carta1.visible=true;
-            this.carta1.position.set(0,0);
-            this.addChild(this.carta1);
+            const carta1=  Sprite.from("alfajor");
+            carta1.scale.set(0.4);
+            carta1.position.set(0,0);
+            carta1.on("mousedown",this.onMouseDown,this);
+            carta1.interactive=true;
+            carta1.visible=true;
+            this.addChild(carta1);
 
-          const back:Graphics=new Graphics();
+            const carta2=  Sprite.from("brigadier");
+            carta2.scale.set(0.4);
+            carta2.position.set(150,0);
+            carta2.on("mousedown",this.onMouseDown,this);
+            carta2.interactive=true;
+            carta2.visible=true;
+            this.addChild(carta2);
+
+            const back:Graphics=new Graphics();
             back.lineStyle(5,0xB5B2B2);
             back.moveTo(0,0);
             back.beginFill(0xB5B2B2,5);
-            back.lineTo(100,0);
-            back.lineTo(100,120);
+            back.lineTo(120,0);
+            back.lineTo(120,120);
             back.lineTo(0,120);
             back.lineTo(0,0);
             back.endFill();
-            back.visible=true;
-           contenedor.addChild(back);
-           contenedor.position.set(100,100);
-           contenedor.interactive=true;
-           contenedor.on("onpointerdown",this.onPointerDown,this);
-           
+            back.on("mousedown",this.onMouseDown,this);
+            back.interactive=true;
+            back.visible=false;
+            contenedor.addChild(back);
+          
         }
-    private onPointerDown():void
-    {
+    private onMouseDown():void
+    { 
         console.log("carta clikeada");
-        /*if (this)
-        {
-            this.visible=!false;
-        }*/
+      /* if (c==true)
+       {
+        carta1.visible=true;
+        back.visible=false;
+       }*/
     }
     }
--- src/pantalla/pantalla1.ts
+++ src/pantalla/pantalla1.ts
@@ -1,13 +1,19 @@
 import { Container, Sprite, Text } from "pixi.js";
+import { HEIGHT, WIDTH } from "..";
 
 export class Pantalla1 extends Container
 {
     constructor(){
         super();
         const bg= Sprite.from("santafe");
+        bg.scale.set(1.5);
         this.addChild(bg);
 
         const texto= new Text("Santa MEMO Fe");
+        texto.anchor.set(0.5);
+        texto.position.set(WIDTH/2,HEIGHT/2);
+        texto.scale.set(3);
+
         this.addChild(texto);
 
        // const btnmenu= new Button();

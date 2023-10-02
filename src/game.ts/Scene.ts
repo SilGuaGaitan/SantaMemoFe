dc62e99adefec62520322f2f98cfdebecd059bd5
SilGuaGaitan <dragonrosadito@gmail.com>
2023-10-01 21:58:06 -0300

    grilla ok

--- src/game.ts/Scene.ts
+++ src/game.ts/Scene.ts
@@ -6,22 +6,22 @@ export class Scene extends Container{
         super();
        
         const mazo:Array<Graphics>=[];
-        for (let i=0;i<20;i++){
+        for (let i=0; i < 20; i++){
             const aux=new Graphics();
-            aux.beginFill(0x0000FF);
-            aux.drawRect(0,0,100,100);
+            aux.beginFill(0xFFFFFF * Math.random());
+            aux.drawRect(0,0,50,50);
             aux.endFill();
             mazo.push(aux);
-            const auxText= new Text(i,{fontSize:50});
+            const auxText= new Text(i,{fontSize:30});
             aux.addChild(auxText);
         
         }
         this.addChild(...mazo);
         let count=0;
-        for(let i=0;i<4;i++){
-            for (let j=0;j<5;j++){
-             mazo[count].x= 50 + i+(mazo[count].width +10);
-             mazo[count].y= 50 + j+(mazo[count].height +10);
+        for(let i=0; i < 4; i++){
+            for (let j=0; j < 5; j++){
+             mazo[count].x = 20 + i * (mazo[count].width + 10);
+             mazo[count].y = 20 + j * (mazo[count].height + 10);
              count ++;
              console.log("contador: ",count);
             }

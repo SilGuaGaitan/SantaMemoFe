295a7ba6a06ba8cd138d15dd78275220f776d7e0
SilGuaGaitan <dragonrosadito@gmail.com>
2023-10-24 15:59:39 -0300

    juego andando

--- src/assets.ts
+++ src/assets.ts
@@ -1,29 +1,40 @@
-export const assets ={
-    santafe:"./imgsantafe/puente-colgante.jpg",
-    0:"./imgsantafe/0.png" ,
-    1:"./imgsantafe/1.png" ,
-    2:"./imgsantafe/2.png" ,
-    3:"./imgsantafe/3.png" ,
-    4:"./imgsantafe/4.png",
-    5:"./imgsantafe/5.png" ,
-    6:"./imgsantafe/6.png",
-    7:"./imgsantafe/7.png",
-    8:"./imgsantafe/8.png",
-    9:"./imgsantafe/9.png",
-    10:"./imgsantafe/10.png",
-    11:"./imgsantafe/11.png",
-    12:"./imgsantafe/12.png",
-    13:"./imgsantafe/13.png",
-    14:"./imgsantafe/14.png",
-    15:"./imgsantafe/15.png",
-    16:"./imgsantafe/16.png",
-    17:"./imgsantafe/17.png",
-    18:"./imgsantafe/18.png",
-    19:"./imgsantafe/19.png",
-    santa:"./imgsantafe/santafe.jpg",
-    back:"./imgsantafe/back.jpg",
-    boing:"./Sonidos/boing.mp3",
-    carta:"./Sonidos/carta.mp3",
-    fiesta:"./Sonidos/fiesta.mp3"
-}
 
+import { ResolverManifest } from "pixi.js";
+
+export const manifest: ResolverManifest = {
+    bundles: [
+        {
+            name: "myBundle",
+            assets:
+            {
+
+                santafe: "./imgsantafe/puente-colgante.jpg",
+                0: "./imgsantafe/0.png",
+                1: "./imgsantafe/1.png",
+                2: "./imgsantafe/2.png",
+                3: "./imgsantafe/3.png",
+                4: "./imgsantafe/4.png",
+                5: "./imgsantafe/5.png",
+                6: "./imgsantafe/6.png",
+                7: "./imgsantafe/7.png",
+                8: "./imgsantafe/8.png",
+                9: "./imgsantafe/9.png",
+                10: "./imgsantafe/10.png",
+                11: "./imgsantafe/11.png",
+                12: "./imgsantafe/12.png",
+                13: "./imgsantafe/13.png",
+                14: "./imgsantafe/14.png",
+                15: "./imgsantafe/15.png",
+                16: "./imgsantafe/16.png",
+                17: "./imgsantafe/17.png",
+                18: "./imgsantafe/18.png",
+                19: "./imgsantafe/19.png",
+                santa: "./imgsantafe/santafe.jpg",
+                back: "./imgsantafe/back.jpg",
+                boing: "./Sonidos/boing.mp3",
+                carta: "./Sonidos/carta.mp3",
+                fiesta: "./Sonidos/fiesta.mp3"
+            }
+        },
+    ]
+}
--- src/game.ts/loaderScene.ts
+++ src/game.ts/loaderScene.ts
@@ -1,49 +1,66 @@
-import { Assets, Graphics } from "pixi.js";
+
+import { Container, Graphics, Assets } from "pixi.js";
+import { manifest } from "../assets";
 import { SceneBase } from "./SceneBase";
-import { SceneManager } from "../game.ts/SceneManager";
-import {  MenuScene } from "../game.ts/menuScene";
-import { assets } from "../assets";
+import { SceneManager } from "./SceneManager";
+import { MenuScene } from "./menuScene";
 
-export class loaderScene extends SceneBase{
-    public update(): void {};
-    public bar:Graphics;
-    constructor()
-    {
+export class loaderScene extends Container implements SceneBase {
+    private loaderBar: Container;
+    private loaderBarBoder: Graphics;
+    private loaderBarFill: Graphics;
+
+    constructor() {
         super();
-        this.bar=new Graphics();
-        this.setBarPercent(10);
 
-        this.bar.x= SceneManager.WIDTH*0.5;
-        this.bar.y=SceneManager.HEIGHT*0.5;
-        this.bar.pivot.x=this.bar.width /2;
-        this.bar.pivot.y=this.bar.height/2;
-        this.addChild(this.bar);
-        this.downloadAssets();
-        
-    }
-    private downloadAssets()
-    {
-        Assets.addBundle("myAssets", assets); 
-        Assets.loadBundle(["myAssets"], this.setBarPercent.bind(this)).then(()=>{
-            this.whenLoaderFinished();
-            
-        });
-    }
-    private setBarPercent(percent:number)
-    {
-        const factor= percent/100;
-        this.bar.clear();
-        this.bar.beginFill(0xFF0000,1);
-        this.bar.drawRect(0,0, SceneManager.WIDTH *0.8 * factor, 50);
-        this.bar.endFill();
-        
-        this.bar.lineStyle(5,0xFFFFFF,1);
-        this.bar.drawRect(0,0, SceneManager.WIDTH *0.8, 50);
-        this.bar.endFill();
+        const loaderBarWidth = SceneManager.WIDTH * 0.8;
 
+        this.loaderBarFill = new Graphics();
+        this.loaderBarFill.beginFill(0xF33302, 1)
+        this.loaderBarFill.drawRect(0, 0, loaderBarWidth, 100);
+        this.loaderBarFill.endFill();
+        this.loaderBarFill.scale.x = 0;
+
+        this.loaderBarBoder = new Graphics();
+        this.loaderBarBoder.lineStyle(5, 0xFFFFFF, 1);
+        this.loaderBarBoder.drawRect(0, 0, loaderBarWidth, 100);
+
+        this.loaderBar = new Container();
+        this.loaderBar.addChild(this.loaderBarFill);
+        this.loaderBar.addChild(this.loaderBarBoder);
+        this.loaderBar.position.x = (SceneManager.WIDTH - this.loaderBar.width) / 2;
+        this.loaderBar.position.y = (SceneManager.HEIGHT - this.loaderBar.height) / 2;
+        this.addChild(this.loaderBar);
+
+       /* this.texty = new Text("CARGANDO", { fontSize: 30, fill: 0xFFFFFF });
+        this.texty.anchor.set(0.5);
+        this.texty.position.set(SceneManager.WIDTH / 2, SceneManager.HEIGHT / 2);
+        this.addChild(this.texty)*/
+
+        this.initializeLoader().then(() => {
+            this.gameLoaded();
+        })
     }
-    private whenLoaderFinished()
-    {
+
+
+
+    private async initializeLoader(): Promise<void> {
+        await Assets.init({ manifest: manifest });
+        const bundleIds = manifest.bundles.map(bundle => bundle.name);
+        await Assets.loadBundle(bundleIds, this.downloadProgress.bind(this));
+    }
+
+    private downloadProgress(progressRatio: number): void {
+        this.loaderBarFill.scale.x = progressRatio;
+    }
+
+    private gameLoaded(): void {
         SceneManager.changeScene(new MenuScene);
     }
+
+    public update(_framesPassed: number): void 
+    {
+    
+    }
 }
+
